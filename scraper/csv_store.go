38c03fe87c945f6763d0245025cbed9b666e27a4
Hannan Solo <Hannanrhodes@gmail.com>
2020-05-23 15:57:14 -0400

    stub file for csv storage

---
+++ scraper/csv_store.go
@@ -0,0 +1,15 @@
+package scraper
+
+import "io"
+
+// create a method that takes in a candle and returns it as a csv write friendly slice
+func (candle Candle) toCSV() []string {
+
+}
+
+//get or create a csv writer based on a ticker csv file
+func NewCSVWriter(w *io.Writer) {
+
+}
+
+//create a ticker to file
